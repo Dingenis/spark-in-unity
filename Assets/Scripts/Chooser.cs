88e9982368e1eb65f6f778546571529225b5966b
Dingenis Sieger Sinke <dingenis.sieger@gmail.com>
2020-05-13 14:46:14 +0200

    Worked on flow system

---
+++ Assets/Scripts/Chooser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class Chooser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<VideoPlayer>().loopPointReached += OnVideoEnded;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnVideoEnded(VideoPlayer player) {
+        bool randomValue = (Random.value >= 0.5);
+
+        if(randomValue) {
+            SceneManager.LoadScene("StratumseindNew");
+        } else {
+            SceneManager.LoadScene("StratumseindNormal");
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+
+
+    public bool FountainClose = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 userPosition = transform.position;
+
+        GameObject fountain = GameObject.Find("Fountain");
+        
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 70)) {
+            Collider collider = hit.collider;
+            GameObject gObject = collider.gameObject;
+            if(gObject.name == "Fountain") {
+                FountainClose = true;
+            } else {
+                FountainClose = false;
+            }
+        } else {
+            FountainClose = false;
+        }
+
+    }
+}
